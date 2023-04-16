e4f48b0f70933a665263024d683d647c096e6b07
Abolfazl <abolfazl2800140@gmail.com>
2023-04-16 10:47:42 +0330

    firstCommit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <div className='right radius'></div>
+      <div className='center radius'></div>
+      <div className='left radius'></div>
+    </div>
+  )
+}
+
+export default App;
