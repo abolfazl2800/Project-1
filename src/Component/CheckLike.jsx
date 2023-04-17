ae8a4ea699097c56ae495a295aa500e11079ebe2
Abolfazl <abolfazl2800140@gmail.com>
2023-04-17 14:39:43 +0330

    checkLike Commit

---
+++ src/Component/CheckLike.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const CheckLike = () => {
+    return(
+        <div>
+            tyjhyjyetjtjmrpokimnrotytrhjytrjyj
+        </div>
+    )
+}
+
+
+export default CheckLike;
